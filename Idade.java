e1a62ead7805019a106978095705aa725af21bd0
jessi@Mayumi <jessicaikuno2@gmail.com>
2023-02-25 11:13:50 -0400

    first commit

---
+++ Idade.java
@@ -0,0 +1,20 @@
+
+public class Idade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int anoAtual;
+		int anoNascimento;
+		int idade;
+		
+		anoAtual = 2023;
+		anoNascimento = 1998;
+		idade = anoAtual -anoNascimento;
+		
+		System.out.println(" A idade atual do usuario é:" + idade);
+		
+
+	}
+
+}
